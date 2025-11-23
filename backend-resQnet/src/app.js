cceeb97eb73732a716af984e3921d15e0e08ca38
mohd-ksr <mohd.ksr2003@gmail.com>
2025-11-23 17:21:06 +0530

    Update CORS configuration to allow new frontend origin

--- backend-resQnet/src/app.js
+++ backend-resQnet/src/app.js
@@ -130,7 +130,7 @@ app.use(cors({
     const allowedOrigins = [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://resqnet-frontend.vercel.app",
+      "https://resqnet-frontend-c9xspncsl-mo-kausars-projects.vercel.app",
       "https://resqnet-admin.vercel.app"
     ];
 
